c318acb464a0ea03b7426104fd1cd1fd9f7d97b7
jdbremer <54557659+jdbremer@users.noreply.github.com>
2021-01-16 14:12:56 -0600

    single connection

---
+++ software/client.py
@@ -0,0 +1,20 @@
+# Import socket module  
+import socket             
+  
+# Create a socket object  
+s = socket.socket()       
+  
+# Define the port on which you want to connect  
+port = 12346                
+  
+# connect to the server on local computer  
+s.connect(('192.168.1.28', port))  
+  
+# receive data from the server  
+print (s.recv(1024) ) 
+
+s.send('Thank') 
+
+print (s.recv(1024) ) 
+# close the connection  
+s.close()
