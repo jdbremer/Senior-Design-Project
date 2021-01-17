83866167d1d8384c96fb2ed70236ece7b4982226
jdbremer <jordan.bremer@yahoo.com>
2021-01-17 14:18:01 -0600

    it now shows disconnects and handles incoming data from multiple clients properly

--- software/client.py
+++ software/client.py
@@ -1,20 +1,20 @@
-# Import socket module  
-import socket             
+# Import socket module
+import socket
 import time
 
-# Create a socket object  
-s = socket.socket()       
-  
-# Define the port on which you want to connect  
-port = 12346                
-  
-# connect to the server on local computer  
-s.connect(('192.168.1.28', port))  
-  
-# receive data from the server  
-print (s.recv(1024) ) 
+# Create a socket object
+s = socket.socket()
 
-s.send('Thank') 
+# Define the port on which you want to connect
+port = 12346
+
+# connect to the server on local computer
+s.connect(('192.168.1.28', port))
+
+# receive data from the server
+print (s.recv(1024) )
+
+s.send('Thanks for connecting')
 time.sleep(2)
 s.send('1234')
 time.sleep(2)
@@ -23,8 +23,12 @@ time.sleep(2)
 s.send('1236')
 time.sleep(2)
 s.send('1237')
+time.sleep(2)
+s.send('1238')
+time.sleep(2)
+s.send('1239')
 
+time.sleep(2)
 
-
-# close the connection  
+# close the connection
 s.close()
--- software/server.py
+++ software/server.py
@@ -1,62 +1,63 @@
-# first of all import the socket library  
-import socket    
-import thread 
-import select        
+# first of all import the socket library
+import socket
+import thread
+import select
 import time
-  
+
 def on_new_client(clientSocket,addr):
     print ('Got connection from', addr )
     clientSocket.send('Thank you for connecting')
     print (clientSocket.recv(1024))
-    while True: 
+    while True:
         try:
-            print (clientSocket.recv(1024))
-        except:
-            print('Socket has disconnected! ', addr)
-            clientSocket.close()
+            print (clientSocket.recv(1024), addr)
+        except socket.error:
+#            print('Socket has disconnected! ', addr)
+#            clientSocket.close()
             return
 
-        
-def clientCloseCheck(clientSocket,addr):
-    time.sleep(10)
-    try:
-        clientSocket.send('Test')
-    except:
-        print('Socket has disconnected ', addr)
-        clientSocket.close()
-        return
-    
-# next create a socket object  
-s = socket.socket()       
-print ("Socket successfully created") 
 
-# reserve a port on your computer in our  
-# case it is 12345 but it can be anything  
-port = 12346                
-  
-# Next bind to the port  
-# we have not typed any ip in the ip field  
-# instead we have inputted an empty string  
-# this makes the server listen to requests  
-# coming from other computers on the network  
-s.bind(('', port))        
-print ("socket binded to %s" %(port))  
-  
-# put the socket into listening mode  
-s.listen(5)   
-print ("socket is listening")            
-  
-# a forever loop until we interrupt it or  
-# an error occurs  
-while True:  
-  
-    # Establish connection with client.  
-    c, addr = s.accept()   
-    
+def clientCloseCheck(clientSocket,addr):
+    while True:
+        time.sleep(1)
+        try:
+            clientSocket.send('Test')
+        except socket.error:
+            print('Socket has disconnected! ', addr)
+            clientSocket.close()
+            break
+
+# next create a socket object
+s = socket.socket()
+print ("Socket successfully created")
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12347
+
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('', port))
+print ("socket binded to %s" %(port))
+
+# put the socket into listening mode
+s.listen(5)
+print ("socket is listening")
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+
+    # Establish connection with client.
+    c, addr = s.accept()
+
     thread.start_new_thread(on_new_client,(c,addr))
     thread.start_new_thread(clientCloseCheck,(c,addr))
-    # send a thank you message to the client.  
-      
-      
-    # Close the connection with the client  
-    #c.close()  
+    # send a thank you message to the client.
+
+
+    # Close the connection with the client
+    #c.close()
