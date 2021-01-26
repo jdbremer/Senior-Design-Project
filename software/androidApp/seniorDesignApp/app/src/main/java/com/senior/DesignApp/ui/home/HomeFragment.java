1ecd0527c03e89ec8e41c3b2fb2eab9d9caeb3de
jdbremer <54557659+jdbremer@users.noreply.github.com>
2021-01-25 22:54:11 -0600

    added ability for app to sense when changes were made within the database

--- software/androidApp/seniorDesignApp/app/src/main/java/com/senior/DesignApp/ui/home/HomeFragment.java
+++ software/androidApp/seniorDesignApp/app/src/main/java/com/senior/DesignApp/ui/home/HomeFragment.java
@@ -24,20 +24,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.quickstart.database.databinding.ActivityNewPostBinding;
-import com.google.firebase.quickstart.database.java.models.Post;
-import com.google.firebase.quickstart.database.java.models.User;
+//import com.google.firebase.quickstart.database.databinding.ActivityNewPostBinding;
+//import com.google.firebase.quickstart.database.java.models.Post;
+//import com.google.firebase.quickstart.database.java.models.User;
 
 import com.senior.DesignApp.R;
 
 public class HomeFragment extends Fragment {
 
     private DatabaseReference mDatabase;
+    private DatabaseReference mPostReference;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("testObj").child("Key2");  //LISTENER OBJECT
+        mDatabase = FirebaseDatabase.getInstance().getReference();  //DATABASE OBJECT
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         //final TextView textView = root.findViewById(R.id.text_home);
@@ -47,17 +49,11 @@ public class HomeFragment extends Fragment {
                 //textView.setText(s);
             }
         });
-        //View v = inflater.inflate(R.layout.fragment_start, container, false);
-
-        //Button button = (Button) View.OnClickListener();
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
 
         Button b = (Button) root.findViewById(R.id.dataBaseSend);
         EditText key1 = (EditText) root.findViewById(R.id.Key1_text);
 
-        Button b2 = (Button) root.findViewById(R.id.dataBaseSend2);
-        EditText key2 = (EditText) root.findViewById(R.id.Key2_text);
-        //Button button = (Button) findViewById(R.id.dataBaseSend);
+
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
@@ -65,19 +61,8 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
-        ValueEventListener postListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                Post post = dataSnapshot.getValue(Post.class);
-                // ...
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-            }
+        Button b2 = (Button) root.findViewById(R.id.dataBaseSend2);
+        EditText key2 = (EditText) root.findViewById(R.id.Key2_text);
 
         b2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -85,6 +70,28 @@ public class HomeFragment extends Fragment {
                 mDatabase.child("testObj").child("Key2").setValue(key2.getText().toString());
             }
         });
+
+
+        //LISTENER CODE//
+        EditText receivedkey2 = (EditText) root.findViewById(R.id.receivedKey2);
+        ValueEventListener postListener = new ValueEventListener(){
+            @Override
+            public void onDataChange (DataSnapshot dataSnapshot){
+                // Get Post object and use the values to update the UI
+                //System.out.println(dataSnapshot.getValue());
+                receivedkey2.setText(dataSnapshot.getValue().toString());
+
+                // ...
+            }
+
+            @Override
+            public void onCancelled (@NonNull DatabaseError error){
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                System.out.println("The read failed: " + error.getMessage());
+            }
+        };
+        mPostReference.addValueEventListener(postListener);
+        //END LISTENER CODE//
 
         return root;
     }
