3bcd1c3441bd467c90bebf6b7b5b20934d52cafb
jdbremer <jordan.bremer@yahoo.com>
2021-01-28 11:34:30 -0600

    ubuntu

--- software/Server/server.py
+++ software/Server/server.py
@@ -3,6 +3,11 @@ import socket
 import _thread
 import select
 import time
+import threading
+
+lock = threading.Lock()
+
+connections = {}
 
 def on_new_client(clientSocket,addr):
     print ('Got connection from', addr )
@@ -11,18 +16,36 @@ def on_new_client(clientSocket,addr):
     print (clientSocket.recv(1024).decode('ascii'))
     while True:
         try:
+            fromClient = clientSocket.recv(1024).decode('ascii')
+            if(fromClient == ' '):
+               clientSocket.close()
+	       return
             print (clientSocket.recv(1024).decode('ascii'), addr)
             checkMsg = 'I am here'
             clientSocket.send(checkMsg.encode('ascii'))
         except socket.error:
 #            print('Socket has disconnected! ', addr)
-#            clientSocket.close()
+            clientSocket.close()
             return
 
 
 def clientCloseCheck(clientSocket,addr):
+    lock.acquire()
+    clientSocket.send('connected....'.encode('ascii'))
+    sensor = clientSocket.recv(1024).decode('ascii')
+    print (sensor)    
+ #   clientSocket.send('send client ID...'.encode('ascii'))
+ #   clientID = clientSocket.recv(1024.decode('ascii'))
+ #   print (clientID)
+#    print(clientID[1])
+    clientID = clientSocket.getsockname()
+    print (clientID[1])
+    connections[clientID[1]].append(sensor)
+
+    lock.release()
+
     while True:
-        time.sleep(1)
+        #time.sleep(1)
         try:
             checkMsg = 'Are you there?'
             clientSocket.send(checkMsg.encode('ascii'))
@@ -32,36 +55,47 @@ def clientCloseCheck(clientSocket,addr):
             break
 
 # next create a socket object
-s = socket.socket()
-print ("Socket successfully created")
+data = socket.socket()
+status = socket.socket()
+print ("Sockets successfully created")
 
 # reserve a port on your computer in our
 # case it is 12345 but it can be anything
-port = 12346
-
+dataPort = 12346
+statusPort = 12347
 # Next bind to the port
 # we have not typed any ip in the ip field
 # instead we have inputted an empty string
 # this makes the server listen to requests
 # coming from other computers on the network
-s.bind(('', port))
-print ("socket binded to %s" %(port))
+data.bind(('', dataPort))
+print ("data socket binded to %s" %(dataPort))
+
+status.bind(('',statusPort))
+print ("status socket binded to %s" %(statusPort))
 
 # put the socket into listening mode
-s.listen(5)
-print ("socket is listening")
+data.listen(5)
+print ("data socket is listening")
+status.listen(5)
+print ("status socket is listening")
 
 # a forever loop until we interrupt it or
 # an error occurs
 while True:
-
+ #   data.listen(5)
+#    print ("data socket is listening")
     # Establish connection with client.
-    c, addr = s.accept()
+    c, addr = data.accept()
 
     _thread.start_new_thread(on_new_client,(c,addr))
-    _thread.start_new_thread(clientCloseCheck,(c,addr))
+    #_thread.start_new_thread(clientCloseCheck,(c,addr))
     # send a thank you message to the client.
-
+  #  status.listen(5)
+  #  print ("status socket is listening")
+    
+    c, addr = status.accept()
+    _thread.start_new_thread(clientCloseCheck, (c,addr))
 
     # Close the connection with the client
     #c.close()
