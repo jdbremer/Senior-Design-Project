e6dee0a3b74b466fb6ea8e904667b81708251141
jdbremer <jordan.bremer@yahoo.com>
2021-01-28 11:36:31 -0600

    ubunut

--- software/Client/client.py
+++ software/Client/client.py
@@ -3,19 +3,33 @@ import socket
 import time
 import _thread
 
+def statusSocket(serverSocket,dataSocket):
+	print (serverSocket.recv(1024).decode('ascii'))
+	serverSocket.send('LightSensor'.encode('ascii'))
+    #    print (serverSocket.recv(1024).decode('ascii')
+    #    serverSocket.send(dataSocket.getsockname().decode('ascii'))
+#	while True:
+#        	continue
+
 # Create a socket object
 s = socket.socket()
-#status = socket.socket()
+status = socket.socket()
 
 # Define the port on which you want to connect
-#port = 12346
+port = 12346
 statusPort = 12347
 
 # connect to the server on local computer
 s.connect(('192.168.1.28', port))
 # connect to the server on local computer
-#status.connect(('192.168.1.28', statusPort))
+status.connect(('192.168.1.28', statusPort))
+
+_thread.start_new_thread(statusSocket,(status, s))
+
 print(s.getsockname())
+
+
+
 # receive data from the server
 print (s.recv(1024).decode('ascii') )
 
@@ -51,3 +65,4 @@ time.sleep(2)
 
 # close the connection
 s.close()
+status.close()
