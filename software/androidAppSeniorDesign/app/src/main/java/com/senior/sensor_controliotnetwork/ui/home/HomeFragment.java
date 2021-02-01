1c3643251fa5c16a175bd86024013364579e5c8f
jdbremer <54557659+jdbremer@users.noreply.github.com>
2021-02-01 15:19:43 -0600

    Tabbed version of app, deleted old apps

---
+++ software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/home/HomeFragment.java
@@ -0,0 +1,72 @@
+package com.senior.sensor_controliotnetwork.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.senior.sensor_controliotnetwork.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        TabLayout tabLayout = root.findViewById(R.id.lightTabBar);
+        TabItem tabData = root.findViewById(R.id.lightData);
+        TabItem tabGraph = root.findViewById(R.id.lightGraph);
+        ViewPager viewPager = root.findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getChildFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+        return root;
+    }
+
+}
---
+++ software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/home/PagerAdapter.java
@@ -0,0 +1,42 @@
+package com.senior.sensor_controliotnetwork.ui.home;
+
+import android.graphics.pdf.PdfDocument;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+
+    public PagerAdapter(FragmentManager fm, int numOfTabs){
+        //super(fm);
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        this.numOfTabs = numOfTabs;
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return new DataFragment();
+
+            case 1:
+                return new GraphFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
