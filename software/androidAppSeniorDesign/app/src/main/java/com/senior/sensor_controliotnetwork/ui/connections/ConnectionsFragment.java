eababdcabe29cd981bc65446c0f3ee1ec92d5620
jdbremer <54557659+jdbremer@users.noreply.github.com>
2021-02-16 23:40:22 -0600

    can now update the list with new connections

--- software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/connections/ConnectionsFragment.java
+++ software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/connections/ConnectionsFragment.java
@@ -19,16 +19,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 import com.senior.sensor_controliotnetwork.MainActivity;
 import com.senior.sensor_controliotnetwork.R;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ConnectionsFragment extends Fragment {
 
     private ArrayAdapter<String> adapter;
     public ArrayList<String> arrayList = new ArrayList<String>();
 
+    public DatabaseReference mDatabase;
+    private DatabaseReference mPostReference;
 
 
     private ConnectionsViewModel connectionsViewModel;
@@ -38,6 +50,11 @@ public class ConnectionsFragment extends Fragment {
         connectionsViewModel =
                 new ViewModelProvider(this).get(ConnectionsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_connections, container, false);
+
+
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("Connections");  //LISTENER OBJECT
+        mDatabase = FirebaseDatabase.getInstance().getReference();  //DATABASE OBJECT
+
 
         ListView connections = root.findViewById(R.id.connectionsList);
 
@@ -61,6 +78,36 @@ public class ConnectionsFragment extends Fragment {
             }
         };
 
+
+        //CONSTANT LISTENER CODE//
+        //EditText receivedkey2 = (EditText) root.findViewById(R.id.receivedKey2);
+        ValueEventListener constantListener = new ValueEventListener(){
+            @Override
+            public void onDataChange (DataSnapshot dataSnapshot){
+                 Iterator<DataSnapshot> iter = dataSnapshot.getChildren().iterator();
+                while (iter.hasNext()){
+                    DataSnapshot snap = iter.next();
+                    String nodId = snap.getKey();
+                    int onOff = Integer.parseInt((String) snap.getValue());
+                    if(onOff == 1){
+                        addingToList(nodId);
+                    }
+                    else{
+                        removeFromList(nodId);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled (@NonNull DatabaseError error){
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                System.out.println("The read failed: " + error.getMessage());
+            }
+        };
+        mPostReference.addValueEventListener(constantListener);  //Uncomment this to start the continous grab of updated data (runs code above, constant listener code)
+        //END CONSTANT LISTENER CODE//
+
+
         connectionsFilter.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -81,42 +128,28 @@ public class ConnectionsFragment extends Fragment {
 
         connections.setAdapter(adapter);
 
-        addingToList();
-
         return root;
     }
 
 
 
-    public void addingToList(){
+    public void removeFromList(String s){
 
-        arrayList.add("I");
-        arrayList.add("Just");
-        arrayList.add("Created");
-        arrayList.add("A");
-        arrayList.add("List");
-        arrayList.add("LETS GOOO");
-        arrayList.add("TEST CHILD NODE CONNECTION \nTime Connected: 15h:32m");
+        if(arrayList.contains(s)){
+            arrayList.remove(s);
+            adapter.notifyDataSetChanged();
+        }
 
-        arrayList.add("Can you SCROLL???");
-        arrayList.add("Lets find out....");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("Adding entry");
-        arrayList.add("YES YOU CAN SCROLL!!!!");
+    }
+
+
+    public void addingToList(String s){
+
+        if(!arrayList.contains(s)){
+            arrayList.add(s);
+            adapter.notifyDataSetChanged();
+        }
+
+
     }
 }
