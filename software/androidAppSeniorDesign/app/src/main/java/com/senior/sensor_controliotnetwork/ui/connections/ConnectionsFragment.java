26f93d76fd288523b8bf6a84e0be5ed3004e1829
jdbremer <54557659+jdbremer@users.noreply.github.com>
2021-02-24 10:36:40 -0600

    nothing

--- software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/connections/ConnectionsFragment.java
+++ software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/connections/ConnectionsFragment.java
@@ -140,9 +140,6 @@ public class ConnectionsFragment extends Fragment {
 
         connectionsFilter.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
