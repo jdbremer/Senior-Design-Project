f5461c1c67c950fc67aa350dce05611a6efb50ad
jdbremer <54557659+jdbremer@users.noreply.github.com>
2021-03-23 21:14:14 -0500

    added a function to detect if alexa changed it's control switch value to 0 or 1, then update various parts of the database accordingly

--- software/firebaseFunctions/functions/index.js
+++ software/firebaseFunctions/functions/index.js
@@ -1,7 +1,19 @@
 const functions = require("firebase-functions");
 
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.makeUppercase = functions.database.ref("/alexa/control switch/data")
+    .onWrite((change, context) => {
+      // uses the ESLint styling, you can look it up
+      // the max length of a line is 80
+      // comments need a space after "//"
+      // there can be no spaces in-between lines
+      // there can not be spaces after any line
+      // needs to be all tab or space
+      const original = change.after.val();
+      const reftwo = change.after.ref.root
+          .child("/dataFromApp/ControlSwitch");
+      if (original == "1") {
+        return reftwo.set("1~1");
+      } else {
+        return reftwo.set("0~0");
+      }
+    });
