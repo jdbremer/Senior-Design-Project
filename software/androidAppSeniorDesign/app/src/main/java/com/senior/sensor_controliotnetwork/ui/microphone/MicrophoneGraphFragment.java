68dde379f3c8cf5e951df6e941362466212fdc5a
Y0rkster18 <sweeneyryan18@gmail.com>
2021-03-25 19:52:43 -0500

    added more to microphone data and microphone graph

--- software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/microphone/MicrophoneGraphFragment.java
+++ software/androidAppSeniorDesign/app/src/main/java/com/senior/sensor_controliotnetwork/ui/microphone/MicrophoneGraphFragment.java
@@ -1,43 +1,58 @@
 package com.senior.sensor_controliotnetwork.ui.microphone;
 
-        import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 
-        import androidx.annotation.NonNull;
-        import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
-        import android.provider.ContactsContract;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-        import com.google.firebase.database.DataSnapshot;
-        import com.google.firebase.database.DatabaseError;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
-        import com.google.firebase.database.ValueEventListener;
-        import com.jjoe64.graphview.GraphView;
-        import com.jjoe64.graphview.series.DataPoint;
-        import com.jjoe64.graphview.series.LineGraphSeries;
-        import com.senior.sensor_controliotnetwork.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.senior.sensor_controliotnetwork.R;
 
-        import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MicrophoneGraphFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.content.IntentFilter;
+
+
+import android.app.Service;
+
 public class MicrophoneGraphFragment extends Fragment {
 
-
+    public  static boolean active = false;
     private DatabaseReference mPostReference;
+
+    private int maxDataPoints = 25;
 
 
     private DataPoint[] values = new DataPoint[50];
     private GraphView graph;
     private LineGraphSeries<DataPoint> mSeries1 = new LineGraphSeries<>();
 
-    private double graph2LastXValue = 0d;
+    private ArrayList<DataPoint> mSeries2 = new ArrayList<>();
+
+    // private Map<String, String> sensorValues = new HashMap<String, String>();
+    GraphLevelReceiver receiver;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -70,6 +85,23 @@ public class MicrophoneGraphFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        active = true;
+        getActivity().registerReceiver(receiver, new IntentFilter("SensorMap"));  //<----Register
+
+//        Intent serviceIntent = new Intent(lightService.class.getName());
+//        serviceIntent.setAction("sendSensorMap")
+//        getContext().startService(serviceIntent);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        active = false;
+        getActivity().unregisterReceiver(receiver);           //<-- Unregister to avoid memoryleak
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,113 +110,96 @@ public class MicrophoneGraphFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        receiver = new GraphLevelReceiver();
     }
+
+
+
+    class GraphLevelReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(intent.getAction().equals("SensorMap"))
+            {
+                HashMap<Integer, String> hashMap = (HashMap<Integer, String>)intent.getSerializableExtra("MAPS");
+                testFunc(hashMap);
+                // Show it in GraphView
+            }
+        }
+    }
+
+
 
 
     int inc = 0;
+    int inc2 = 0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
+        // Inflate the layout for this fragment
 
-            return inflater.inflate(R.layout.fragment_microphone_graph, container, false);
+        View root = inflater.inflate(R.layout.fragment_microphone_graph, container, false);
+
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("dataFromChild").child("dBMeter");  //LISTENER OBJECT
+
+        graph = (GraphView) root.findViewById(R.id.dBMetergraph);
+
+        mSeries1 = new LineGraphSeries<>();
+        mSeries1.setThickness(15);
+        mSeries1.setColor(Color.rgb(210,180,140));
+        graph.addSeries(mSeries1);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(1);
+        graph.getViewport().setMaxX(maxDataPoints);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(1000);
+
+        // mSeries1 = new LineGraphSeries<>();
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+        double x = 20;
+        return root;
     }
-//
-//
-//
-//
-//
-//        mPostReference = FirebaseDatabase.getInstance().getReference().child("dataFromChild").child("LightSensor");  //LISTENER OBJECT
-//
-//
-//
-//
-//        graph = (GraphView) root.findViewById(R.id.graph);
-////        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-////                new DataPoint(0, 1),
-////                new DataPoint(1, 5),
-////                new DataPoint(2, 3),
-////                new DataPoint(3, 2),
-////                new DataPoint(4, 6)
-////        });
-////        graph.addSeries(series);
-////        graph.removeSeries(series);
-//
-//
-//        mSeries1 = new LineGraphSeries<>();
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-//
-//
-//        double x = 20;
-//
-//
-//        //double y = 50;
-//        //DataPoint v = new DataPoint(x,y);
-//        //values[inc++] = v;
-//        //mSeries1 = new LineGraphSeries<>();
-////        graph.addSeries(mSeries1);
-////        mSeries1.appendData(new DataPoint(x,y),true,3);
-////        mSeries1.appendData(new DataPoint(25,50),true,3);
-////        mSeries1.appendData(new DataPoint(28,60),true,3);
-////        mSeries1.appendData(new DataPoint(30,70),true,3);
-////        mSeries1.appendData(new DataPoint(50,80),true,3);
-//
-//
-//
-//
-//        //CONSTANT LISTENER CODE//
-//        ValueEventListener constantListener = new ValueEventListener(){
-//            @Override
-//            public void onDataChange (DataSnapshot dataSnapshot){
-//                graph.getViewport().setMinX(0);
-//                graph.getViewport().setMaxX(50);
-//                graph.getViewport().setMinY(0);
-//                graph.getViewport().setMaxY(10);
-//                graph.addSeries(mSeries1);
-//                int maxGraphPoints = 5;
-//                //double x = inc++;
-////                if(x > maxGraphPoints){
-////                    x = 0;
-////                    inc = 0;
-////                }
-//                // double y = 30+inc;
-//
-//
-//
-//
-//                graph2LastXValue += 1d;
-//                //double y = 50;
-//
-////                mSeries1.appendData(new DataPoint(x,y),true,3);
-////                mSeries1.appendData(new DataPoint(25,50),true,3);
-////                mSeries1.appendData(new DataPoint(28,60),true,3);
-////                mSeries1.appendData(new DataPoint(30,70),true,3);
-////                mSeries1.appendData(new DataPoint(50,80),true,3);
-//
-//                double y = Double.parseDouble((String) dataSnapshot.getValue());
-//                mSeries1.appendData(new DataPoint(graph2LastXValue,y*1000),true, maxGraphPoints);
-////                DataPoint v = new DataPoint(x,y);
-////                values[inc++] = v;
-////                mSeries1 = new LineGraphSeries<>(values);
-////                graph.addSeries(mSeries1);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled (@NonNull DatabaseError error){
-//                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-//                System.out.println("The read failed: " + error.getMessage());
-//            }
-//        };
-//        mPostReference.addValueEventListener(constantListener);  //Uncomment this to start the continuous grab of updated data (runs code above, constant listener code)
-//        //END CONSTANT LISTENER CODE//
-//
-//
-//
-//
-//        return root;
-//    }
 
 
 
+//    BroadcastReceiver br = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            HashMap<String, String> hashMap = (HashMap<String, String>)intent.getSerializableExtra("MAPS");
+//        }
+//    };
+
+    public void testFunc(HashMap sensorValues){
+
+        //trying to remove old data on the graph and add new data
+        mSeries1.clearReference(graph);
+        graph.removeSeries(mSeries1);
+        mSeries1 = new LineGraphSeries<>();
+        //LineGraphSeries<DataPoint> mSeries1 = new LineGraphSeries<>();
+        mSeries1.setThickness(15);
+        mSeries1.setColor(Color.rgb(210,180,140));
+        graph.addSeries(mSeries1);
+        graph.refreshDrawableState();
+        graph.onDataChanged(true, true);
+
+
+        //this is here as a test to just continue adding data
+        //mSeries1.appendData(new DataPoint(inc2++,y*1000),false, 10);
+
+
+        TreeMap<Integer,String> sortedSensorValues = new TreeMap<Integer,String>(sensorValues); //convert the hashmaps (which aren't sorted) to treemaps (which are sorted)
+        //iterate through the hash table to then add the shifted data to the series
+        Iterator<TreeMap.Entry<Integer, String>> itr = sortedSensorValues.entrySet().iterator();
+        while(itr.hasNext()){
+            //iterate.next();
+            TreeMap.Entry<Integer, String> entry = itr.next();
+            int x = entry.getKey();
+            double y2 = (Double.parseDouble((String) entry.getValue()));
+            mSeries1.appendData(new DataPoint(x,y2),false, maxDataPoints);
+            graph.refreshDrawableState();
+            graph.onDataChanged(true, true);
+        }
+    }
 }
