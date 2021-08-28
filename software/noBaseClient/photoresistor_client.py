0d573930d2b396dc9a97e7149cc0d834e38e60bc
Jordan <jordan.bremer@yahoo.com>
2021-08-28 14:12:03 -0500

    new cryptography for token.txt file

--- software/noBaseClient/photoresistor_client.py
+++ software/noBaseClient/photoresistor_client.py
@@ -14,12 +14,119 @@ import nexmo
 import pyrebase
 
 
+# For cryptography operations
+# Use 'pip install cryptography' if the library isn't found
+from cryptography.fernet import Fernet
+
+
+
+
+
+tokenFileName = "token.txt"
+keyFileName = "tokenFileKey.key"
+key = ""
+
+
+interval = 5  #default of 5 seconds
+average = 0
+average_lux = 0
+
+
+
 #used for firebase handler
 firstHandlerEntry = 0
-
 token = ""
-
 deviceName = "LightSensor"
+
+
+
+
+#####
+# Using cryptography, we are under the impression that if a tokenFileKey.key file
+# has been made, that the contents are assumed to be encrypted within token.txt.
+# This means that the program should not encrypt the data within token.txt
+# if tokenFileKey.key has been created.
+
+# If the user uses bluetooth to change the wifi ssid password, the token.txt 
+# file needs to be encrypted after modification.
+
+
+# Encrypt the entire file contents
+def encryptFile(fileName, key):
+    # Using the generated key
+    fernet = Fernet(key)
+
+    # Opening the original file to encrypt
+    with open(fileName, 'rb') as file:
+        original = file.read()
+        
+    # Encrypting the file
+    encrypted = fernet.encrypt(original)
+
+    # Epening the file in write mode and
+    # Writing the encrypted data
+    with open(fileName, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+# For decrypting file contents, not the file itself
+def decryptFileContents(fileName, key):
+    # Using the key
+    fernet = Fernet(key)
+
+    # Opening the encrypted file
+    with open(fileName, 'rb') as enc_file:
+        encrypted = enc_file.read()
+
+    # Decrypting the file
+    decrypted = fernet.decrypt(encrypted)
+
+    return decrypted
+    # For debug
+    # print(decrypted)
+
+
+
+if (os.path.exists(tokenFileName)):
+    # If the keyFileName exists it is assumed
+    # that the token.txt file has been already
+    # incrypted or will be encrypted later
+    if os.path.exists(keyFileName):
+        # Opening the key
+        # 'rb' means to open and read binary
+        with open(keyFileName, 'rb') as tokenFileKey:
+            key = tokenFileKey.read()
+
+    else:
+        # Generates the key
+        key = Fernet.generate_key()
+
+        # String the key in a file
+        # 'wb' means to open and write binary
+        with open(keyFileName, 'wb') as tokenFileKey:
+            tokenFileKey.write(key)
+
+        encryptFile(tokenFileName, key)
+
+else:
+    if os.path.exists(keyFileName):
+        os.remove(keyFileName)
+    
+    # If the token.txt file does not exist, the 
+    # user will need to use bluetooth to connect
+    # to the pi and send the token aka. turn on
+    # bluetooth and wait for user input
+    print("token.txt does not exist")
+
+    # JUMP TO BLUETOOTH INIT HERE
+
+
+# Used to decrypt the file, we only want to decrypt the contents
+
+# Opening the file in write mode and
+# Writing the decrypted data
+# with open('token.txt', 'wb') as dec_file:
+# 	dec_file.write(decrypted)
+
 
 
 ##DATABASE INIT##
@@ -42,7 +149,6 @@ auth = firebase.auth()
 
 #firebase listener "dataFromApp"
 def firebaseStreamHandler(event):
-    global token
     global firstHandlerEntry
     global interval
 
@@ -62,24 +168,12 @@ def firebaseStreamHandler(event):
             
 
 
-
-
-
-interval = 5  #default of 5 seconds
-average = 0
-average_lux = 0
-
-
 #function to send data to the server in a sequence
 def sendingToDatabase(data):
-    global token
-    #send the data to the server
     #send the data to the database
-    database.child(token + "/dataFromChild").update({str(deviceName) : str(data)})
+    database.child(decryptFileContents(tokenFileName, key) + "/dataFromChild").update({str(deviceName) : str(data)})
 
     
-    
-
         
 def sendSampleThread(sendSocket,receive):
     global interval
@@ -106,25 +200,28 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
 
-#while loop for firebase token
-while True:
-    if(os.path.exists("token.txt")): #check if the token txt file exists
-        print("Token file exists")
-        grabToken = open("token.txt")   #open token text file
-        token = grabToken.read().replace("\n", " ") #assign token the string from text file without \n
-        grabToken.close     #done grabbing the data from token text file
-        users = database.child(token + "/").get()
-        if users.val() == None:
-            print("Invalid token")
-            time.sleep(3)   #token not found in db, repeat loop after 3 seconds
-        else:
-            print("Token exists")   #token exists in db
-            #update the database to display connected sensor
-            database.child(token + "/Connections").update({str(deviceName) : "1"})
-            break                   #exit while true loop since the token exists
+# Find the firebase token, and verify it is correct
+if(os.path.exists(tokenFileName)): #check if the token txt file exists
+    print("Token file exists")
+    # grabToken = open("token.txt")   #open token text file
+    # token = grabToken.read().replace("\n", " ") #assign token the string from text file without \n
+    # grabToken.close     #done grabbing the data from token text file
+    users = database.child(decryptFileContents(tokenFileName, key) + "/").get()
+    if users.val() == None:
+        print("Invalid token")
+        # JUMP TO BLUETOOTH INIT HERE
+    else:
+        print("Token exists")   #token exists in db
+        #update the database to display connected sensor
+        database.child(decryptFileContents(tokenFileName, key) + "/Connections").update({str(deviceName) : "1"})
+        break                   #exit while true loop since the token exists
+
+# else:
+    # JUMP TO BLUETOOTH INIT HERE
+
 
 #initialize the firebase listener
-myStream = database.child(token + "/dataFromApp/" + deviceName).stream(firebaseStreamHandler, None)
+myStream = database.child(decryptFileContents(tokenFileName, key) + "/dataFromApp/" + deviceName).stream(firebaseStreamHandler, None)
 
 
 
@@ -182,7 +279,7 @@ finally:
     print("clean up")
     GPIO.cleanup()
     #update the database to display connected sensor
-    database.child(token + "/Connections").update({str(deviceName) : "0"})
+    database.child(decryptFileContents(tokenFileName, key) + "/Connections").update({str(deviceName) : "0"})
 
 
 #delay before closing connections
