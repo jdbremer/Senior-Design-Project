e769b1887d1e8b79e6742bd7906c3b6bdf3cbfdb
Y0rkster18 <ryansweeney18@gmail.com>
2021-11-02 17:01:42 -0500

    1st try at mic

--- software/noBaseClient/microphone_client.py
+++ software/noBaseClient/microphone_client.py
@@ -3,92 +3,457 @@ import RPi.GPIO as GPIO #pulls in the GPIO pin numbers
 import socket
 import time
 import _thread
+import math
+import os
+import serial
+import requests
 
-import Adafruit_GPIO.SPI as SPI #ADC SPI library
-import Adafruit_MCP3008
 
-import nexmo
+import busio
+import digitalio
+import board
+# import adafruit_mcp3xxx.mcp3008 as MCP
+# from adafruit_mcp3xxx.analog_in import AnalogIn
 
-ip = '172.20.10.11'
+# create the spi bus
+# spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-#ip = '192.168.86.31'
+# create the cs (chip select)
+# cs = digitalio.DigitalInOut(board.D5)
 
-# client = nexmo.Client(key='77d3ed4c', secret='SjSjkdsIgYw1AHce')
+# create the mcp object
+# mcp = MCP.MCP3008(spi, cs)
 
-# client.send_message({
-#     'from': '18553182827',
-#     'to': '19522327269',
-#     'text': 'Hello from Vonage SMS API',
-# })
+# create an analog input channel on pin 0
+# chan = AnalogIn(mcp, MCP.P0)
+
+
+
+from uuid import getnode as get_mac
+
+# import Adafruit_GPIO.SPI as SPI #ADC SPI library
+# import Adafruit_MCP3008
+
+# import nexmo
+
+import pyrebase
+
+
+# For cryptography operations
+# Use 'pip install cryptography' if the library isn't found
+from cryptography.fernet import Fernet
+
+
+mac = get_mac()
+print("MAC address: " + str(mac))
+
+
+tokenFileName = "/home/pi/Desktop/Senior-Design-project/software/Token/token.txt"
+keyFileName = "/home/pi/Desktop/Senior-Design-project/software/Token/tokenFileKey.key"
+key = ""
 
 
 interval = 5  #default of 5 seconds
 average = 0
+risingEdgeCount = 0
+
+
+
+#used for firebase handler
+firstHandlerEntry = 0
+token = ""
+deviceName = "dBMeter"
+
+
+
+
+#setup the bluetooth config.. this does not include timeout
+#serialPort = serial.Serial("/dev/serial0", baudrate=9600)
+
+appValues = {}
+
+line = []
+fullString = ""
+
+
+grabToken = ""
+token = ""
+
+
+
+
+#BLE Init
+BLEReceived = True
+stopBLEThread = False
+
+
+
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(36, GPIO.OUT) #GREEN LED
+#GPIO.setup(38, GPIO.OUT) #RED LED
+#GPIO.setup(40, GPIO.OUT) #BLE ON/OFF
+
+#GPIO defaults
+#GPIO.output(36, GPIO.HIGH) #GREEN LED
+#GPIO.output(38, GPIO.HIGH) #RED LED
+#GPIO.output(40, GPIO.HIGH)  #BLE ON/OFF
+
+
+
+#####
+# Using cryptography, we are under the impression that if a tokenFileKey.key file
+# has been made, that the contents are assumed to be encrypted within token.txt.
+# This means that the program should not encrypt the data within token.txt
+# if tokenFileKey.key has been created.
+
+# If the user uses bluetooth to change the wifi ssid password, the token.txt 
+# file needs to be encrypted after modification.
+
+
+# Encrypt the entire file contents
+def encryptFile(fileName, key):
+    # Using the generated key
+    fernet = Fernet(key)
+
+    # Opening the original file to encrypt
+    with open(fileName, 'rb') as file:
+        original = file.read()
+        
+    # Encrypting the file
+    encrypted = fernet.encrypt(original)
+
+    # Epening the file in write mode and
+    # Writing the encrypted data
+    with open(fileName, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+# For decrypting file contents, not the file itself
+def decryptFileContents(fileName, key):
+    # Using the key
+    fernet = Fernet(key)
+
+    # Opening the encrypted file
+    with open(fileName, 'rb') as enc_file:
+        encrypted = enc_file.read()
+
+    # Decrypting the file
+    decrypted = fernet.decrypt(encrypted)
+
+    return decrypted
+    # For debug
+    # print(decrypted)
+
+
+
+if (os.path.exists(tokenFileName)):
+    # If the keyFileName exists it is assumed
+    # that the token.txt file has been already
+    # incrypted or will be encrypted later
+    if os.path.exists(keyFileName):
+        # Opening the key
+        # 'rb' means to open and read binary
+        with open(keyFileName, 'rb') as tokenFileKey:
+            key = tokenFileKey.read()
+
+    else:
+        # Generates the key
+        key = Fernet.generate_key()
+
+        # String the key in a file
+        # 'wb' means to open and write binary
+        with open(keyFileName, 'wb') as tokenFileKey:
+            tokenFileKey.write(key)
+
+        encryptFile(tokenFileName, key)
+
+else:
+    if os.path.exists(keyFileName):
+        os.remove(keyFileName)
+    
+    # If the token.txt file does not exist, the 
+    # user will need to use bluetooth to connect
+    # to the pi and send the token aka. turn on
+    # bluetooth and wait for user input
+    print("token.txt does not exist")
+
+
+
+
+######
+#BLE Init
+
+BLEReceived = True
+stopBLEThread = False
+
+
+def BLEModuleInit(fun,fun1):
+    line = []
+    BLEInit = 0
+    global BLEReceived
+    while stopBLEThread == False:
+        for c in serialPort.read().decode():
+            line.append(c)
+            linev2 = ''.join(line).replace("\n", " ").strip()
+            if linev2 == "OK+Set:1":
+                print(linev2)
+                line = []
+                BLEReceived = True
+            elif linev2 == "OK+RESET":
+                print(linev2)
+                line = []
+                BLEReceived = True
+            elif linev2 == "OK+Set:0":
+                print(linev2)
+                line = []
+                BLEReceived = True
+            elif linev2 == "OK+RESET":
+                print(linev2)
+                line = []
+                BLEReceived = True
+            elif linev2 == "OK+Set:SERVER_IoT":
+                print(linev2)
+                line = []
+                BLEReceived = True
+            elif linev2 == "OK" and BLEInit == 0:
+                BLEInit = 1
+                print(linev2)
+                line = []
+                BLEReceived = True
+
+# _thread.start_new_thread(BLEModuleInit,(1,1)) #start thread for BLE init
+
+# BLEReceived = False
+# serialPort.write(("AT").encode())
+# while BLEReceived == False: continue
+# BLEReceived = False
+# serialPort.write(("AT+IMME1").encode())
+# while BLEReceived == False: continue
+# BLEReceived = False
+# serialPort.write(("AT+NAMESERVER_IoT").encode())
+# while BLEReceived == False: continue
+# BLEReceived = False
+# serialPort.write(("AT+IMME0").encode())
+# while BLEReceived == False: continue
+# BLEReceived = False
+# serialPort.write(("AT+RESET").encode())
+# while BLEReceived == False: continue
+
+
+# stopBLEThread = True
+# print("BLE Initialization Complete")
+
+#END BLE Init
+
+
+
+
+
+def runReadSequence():
+    global line
+    global fullString
+    startMsg = "start"
+    endMsg = "stop"
+    ssid = "ssid"
+    ssid_pswd = "password"
+    uid = "uid"
+    dataStart = False
+    dataStop = False
+    line = []
+    while True:
+        for c in serialPort.read().decode():
+            line.append(c)
+            if c == '\n':
+                print("newString")
+                fullString = ''.join(line).replace("\n"," ")
+                print(fullString)
+                line = []
+
+                if startMsg in fullString:
+                    dataStart = True
+                    dataStop = False
+                    print("start")
+                elif endMsg in fullString:
+                    dataStart = False
+                    dataStop = True
+                    print("end")
+                    return
+                elif dataStart == True and dataStop == False:
+                    if ssid in fullString or ssid_pswd in fullString or uid in fullString:
+                        try:
+                            valName = fullString.split(':')[0]
+                            print(valName)
+                            valData = fullString.split(':')[1]
+                            print(valData)
+                            appValues[valName] = valData
+                        except:
+                            print("invalid data")
+                            return
+
+
+
+#restarts the wifi services
+def RestartWifi():
+  os.system('sudo systemctl daemon-reload')
+  time.sleep(5)
+  os.system('sudo systemctl stop dhcpcd.service')
+  time.sleep(5)
+  os.system('sudo systemctl start dhcpcd.service')
+  time.sleep(20)
+
+
+def modifyWPAFile():
+    wifiConfig = open("/etc/wpa_supplicant/wpa_supplicant.conf", "r+")
+    fileContents = wifiConfig.readlines()
+    newFileContents = ""
+    currentFileContents = ""
+    for line in fileContents:
+        newLine = line
+        if "ssid" in line:
+            newLine = "        ssid=" + '"'  + appValues.get("ssid").strip() + '"' + "\n"
+            print(newLine)
+        elif "psk" in line:
+            newLine = "        psk=" + '"' + appValues.get("password").strip() + '"' + "\n"
+            print(newLine)
+        newFileContents += newLine
+    wifiConfig.seek(0)
+    wifiConfig.truncate(0)
+    wifiConfig.write(newFileContents)
+    wifiConfig.close()
+    
+    
+    
+def modifyTOKENFile():
+    tokenConfig = open("/home/pi/Desktop/Senior-Design-Project/software/Token/token.txt", "r+")
+    tokenConfig.seek(0)
+    tokenConfig.truncate(0)
+    tokenConfig.write(appValues.get("uid").strip())
+    tokenConfig.close()
+
+######
+
+
+
+
+def bluetoothMAIN():
+    print("Bluetooth MAIN")
+    internet = False
+    status = ""
+    #check if the pi is connected to the internet'
+    #while internet == False:
+    try:
+        url = "https://www.google.com"
+        #urllib.request.urlopen(url)
+        response = requests.get(url)
+        internet = True
+        #GPIO.output(18, GPIO.LOW)
+        GPIO.output(36, GPIO.HIGH) #GREEN LED
+        GPIO.output(38, GPIO.LOW) #RED LED
+        GPIO.output(40, GPIO.LOW)  #BLE ON/OFF
+        status = "Connected"
+    except requests.ConnectionError:
+        #print(response.status_code)
+        status = "Not connected"
+            
+            
+            
+    print(status)
+    if status == "Not connected":
+        #turn on the bluetooth HAT
+        #GPIO.output(18, GPIO.HIGH)
+        #time.sleep(2)
+        GPIO.output(36, GPIO.LOW) #GREEN LED
+        GPIO.output(38, GPIO.HIGH) #RED LED
+        GPIO.output(40, GPIO.HIGH)  #BLE ON/OFF
+        runReadSequence()
+        modifyWPAFile()
+        modifyTOKENFile()
+        RestartWifi()
+        bluetoothMAIN()
+
+
+
+
+# Used to decrypt the file, we only want to decrypt the contents
+
+# Opening the file in write mode and
+# Writing the decrypted data
+# with open('token.txt', 'wb') as dec_file:
+#   dec_file.write(decrypted)
+
+
+
+##DATABASE INIT##
+#firebase database config
+config = {
+    "apiKey": "AIzaSyAcaqrqFZYmvcAb0qFCI9N4QiZ6L6OeuZ8",
+    "authDomain": "seniordesign-ajr.firebaseapp.com",
+    "databaseURL": "https://seniordesign-ajr-default-rtdb.firebaseio.com",
+    "storageBucket": "seniordesign-ajr.appspot.com",
+}
+
+#initialize the pyrebase config instance 
+firebase = pyrebase.initialize_app(config)
+
+#instantiate both the storage and database firebase libraries
+storage = firebase.storage()
+database = firebase.database()
+auth = firebase.auth()
+##END DATABASE##
+
+#firebase listener "dataFromApp"
+def firebaseStreamHandler(event):
+    global firstHandlerEntry
+    global interval
+
+    #if this is the first time in here, the data will be initialization data, which we want to discard
+    if(firstHandlerEntry == 0):
+        firstHandlerEntry = 1
+
+    else:
+        eventPathString = event["path"]
+        #pulls out the sensor data from the event data, this data is not delimited
+        dataReceivedFromDatabase = eventPathString = event["data"]
+        #CODE TO DO SOMETHING WITH RECEIVED DATA
+        print("dataReceivedFromDatabase: " + str(dataReceivedFromDatabase))
+        interval = int(dataReceivedFromDatabase)
+        print(interval)
+        #END CODE TO DO SOMETHING WITH RECEIVED DATA
+            
 
 
 #function to send data to the server in a sequence
-def sendingSocket(sendingSocket, data):
-     #send the data to the server
-     sendingSocket.send(str(data).encode('ascii'))
-     #received message from server to keep in sync
-     msgFromServer = sendingSocket.recv(1024).decode('ascii')
+def sendingToDatabase(data):
+    #send the data to the database
+    database.child((decryptFileContents(tokenFileName, key)).decode("utf-8") + "/dataFromChild").update({str(deviceName) : str(data)})
 
-
-#thread that initiates when the status socket gets initiated
-def statusSocket(serverSocket,receiveSocket, sendingSocket):
-  print (serverSocket.recv(1024).decode('ascii'))
-  serverSocket.send('dBMeter'.encode('ascii'))
-  
-  
-#thread to handle the data that is received from the base node
-def receivingSocket(serverSocket,receiveSocket, sendingSocket):
-  while True:
-    #data that comes from the base node will end up in receivedDAta
-    receivedData = receiveSocket.recv(1024).decode('ascii')
-    print (receivedData)
-    #need to send data back to keep sync
-    receiveSocket.send('Received...'.encode('ascii'))
-
-    #CODE TO DO SOMETHING WITH RECEIVED DATA
-    interval = int(receivedData)
-
-    #END CODE TO DO SOMETHING WITH RECEIVED DATA
+    
         
-def sampleThread(sendSocket,receive):
+def sendSampleThread(sendSocket,receive):
+    global risingEdgeCount
+
     while True:
-        time.sleep(interval)
-        sendingSocket(sendSocket, average)
+        # time.sleep(interval)
+        print("sending risingEdgeCount to database, this will become dB: " + str(risingEdgeCount))
+        sendingToDatabase(risingEdgeCount)
 
 
-#create a socket object for the receiving, sending, and status sockets
-receiving = socket.socket()
-sending = socket.socket()
-status = socket.socket()
+# Define a threaded callback function to run in another thread when events are detected  
+def my_callback(channel): #enters each time rising edge occurs
+    global risingEdgeCount
 
-#defining of the ports for each of the sockets
-sendPort = 12350
-recvPort = 12351
-statusPort = 12352
+    risingEdgeCount += 1
 
-#connect the IP and the port # to the sockets
-# sending.connect(('192.168.86.31', sendPort))
-# receiving.connect(('192.168.86.31', recvPort))
-# status.connect(('192.168.86.31', statusPort))
+    # if GPIO.input(25):     # if port 25 == 1
+        # print "Rising edge detected on 25"
+        # risingEdgeCount += 1
 
-sending.connect((ip, sendPort))
-receiving.connect((ip, recvPort))
-status.connect((ip, statusPort))
-
-#after connection, start the new status socket thread to handle transmissions
-_thread.start_new_thread(statusSocket,(status, receiving, sending))
-_thread.start_new_thread(receivingSocket,(status, receiving, sending))
 
 
 #hardware SPI configuration
 SPI_PORT   = 0
 SPI_DEVICE = 0
 #connects the SPI port and device to the variable
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 #set the GPIO to the board layout (used for pin numbers)
 # GPIO.setmode(GPIO.BOARD)
@@ -99,60 +464,101 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
 
-#the sending intialization sequence
-#receive, send, then receive again before sending data
-print (sending.recv(1024).decode('ascii') )
-msg = 'Connection Successful..'
-sending.send(msg.encode('ascii'))
-print (sending.recv(1024).decode('ascii') )
-#end of the send initialization sequence
+# Find the firebase token, and verify it is correct
+if(os.path.exists(tokenFileName)): #check if the token txt file exists
+    print("Token file exists")
+    users = database.child((decryptFileContents(tokenFileName, key)).decode("utf-8") + "/").get()
+    if users.val() == None:
+        print("Invalid token")
+        # JUMP TO BLUETOOTH INIT HERE
+    else:
+        print("Token exists")   #token exists in db
+        #update the database to display connected sensor
+        database.child((decryptFileContents(tokenFileName, key)).decode("utf-8") + "/Connections").update({str(deviceName) : "1"})
+
+# else:
+    # JUMP TO BLUETOOTH INIT HERE
+
+
+#initialize the firebase listener
+myStream = database.child((decryptFileContents(tokenFileName, key)).decode("utf-8") + "/dataFromApp/" + deviceName).stream(firebaseStreamHandler, None)
+
 
 
 inc = 0
 average = 0
 numberOfSamples = 500
 sensorTotal = 0
+adcValue = 0
+
+GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
+GPIO.setup(25, GPIO.IN)    # set GPIO25 as input
+GPIO.add_event_detect(25, GPIO.RISING, callback=my_callback) #detect rising edges
+
 #sensor code
-while True:
-  print("inside outter while loop")
-  #grab the start time
-  #start = time.time()
-  #set the pin 18 to high
-  # GPIO.output(18, GPIO.HIGH)
+try:
+    while True:
+        #grab the start time
+        #start = time.time()
+        #set the pin 18 to high
+        # GPIO.output(18, GPIO.HIGH)
 
-  
-  sensorTotal = 0 #reset sensorTotal for next group of samples
-  inc = 0
-    
-  _thread.start_new_thread(sampleThread,(sending,receiving))
-  while True:
-    sensorTotal += mcp.read_adc(0) #read adc value of channel 0
-    #take the average of the value
-    #increment the incrementor
-    inc = inc+1
-    #if the incrementor is greater than 50, enough samples have been taken
-    if(inc > numberOfSamples):
-      #print the average to serial
-      print("inside if statement")
-      average = sensorTotal / numberOfSamples
-      print(average)
-      inc = 0
-      sensorTotal = 0
+        
+        sensorTotal = 0
+        inc = 0
+        
+        sending = 0 #REMOVE
+        receiving = 0 #REMOVE
+        edgeDetectionInterval = 2
 
-       
-# except KeyboardInterrupt:
-#   print("keyboard interrupt")
+        #start the thread to send the average lux on a user specified interval
+        _thread.start_new_thread(sendSampleThread,(sending,receiving)) 
+        while True:
+            sleep(edgeDetectionInterval) #time to allow edge detection
+            print("# of rising edges detected = " + str(risingEdgeCount))
+            risingEdgeCount = 0 #reset counter
 
-# finally:
-#   print("clean up")
-#   GPIO.cleanup()
+            # print('Raw ADC Value: ', chan.value)
+            # print('ADC Voltage: ' + str(chan.voltage) + 'V')
 
 
 
-#delay before closing connections
+
+            # sensorTotal += mcp.read_adc(0) #read adc value of channel 0
+            # sensorTotal += chan.value
+            #take the average of the value
+            #increment the incrementor
+            # inc = inc+1
+            #if the incrementor is greater than the numberOfSamples, enough samples have been taken
+            # if(inc > numberOfSamples):
+            
+                #https://learn.adafruit.com/photocells/using-a-photocell
+                
+                #divide the sensor total by the total number of samples to get the average
+                # adcValue  = sensorTotal / numberOfSamples
+                # if adcValue < 65450:
+                    # isFire = 1
+                # else:
+                    # isFire = 0
+                #use the generated equation to determine the average lux
+                # average_lux = math.e**(((100*adcValue)-23529)/(11996))
+                #round the average 2 decimal places
+                # average_lux = round(average_lux, 2)
+            
+                # inc = 0
+                # sensorTotal = 0
+
+           
+except KeyboardInterrupt:
+    print("keyboard interrupt")
+
+finally:
+    print("clean up")
+    GPIO.cleanup()
+    #update the database to display connected sensor
+    database.child((decryptFileContents(tokenFileName, key)).decode("utf-8") + "/Connections").update({str(deviceName) : "0"})
+    print("connection closed")
+
+
+#delay
 time.sleep(2)
-
-# close the connections
-sending.close()
-receiving.close()
-status.close()
