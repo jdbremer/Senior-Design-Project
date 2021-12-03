8c81cf6d87c80d706c6aae46afb09670d49cec5f
Jordan <jordan.bremer@yahoo.com>
2021-12-02 18:10:22 -0600

    updated Client_template

--- software/noBaseClient/Client_template.py
+++ software/noBaseClient/Client_template.py
@@ -57,13 +57,10 @@ average = 0
 average_lux = 0
 
 
-
 #used for firebase handler
 firstHandlerEntry = 0
 token = ""
 deviceName = "LightSensor"
-
-
 
 
 #setup the bluetooth config.. this does not include timeout
@@ -79,7 +76,6 @@ grabToken = ""
 token = ""
 
 #GPIO Setup
-
 greenLed = digitalio.DigitalInOut(board.D16)
 redLed = digitalio.DigitalInOut(board.D20)
 ble = digitalio.DigitalInOut(board.D21)
@@ -88,11 +84,6 @@ greenLed.direction = digitalio.Direction.OUTPUT
 redLed.direction = digitalio.Direction.OUTPUT
 ble.direction = digitalio.Direction.OUTPUT
 
-
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(36, GPIO.OUT) #GREEN LED
-# GPIO.setup(38, GPIO.OUT) #RED LED
-# GPIO.setup(40, GPIO.OUT) #BLE ON/OFF
 
 ble.value = False  #BLE ON/OFF
 time.sleep(2) # We want the bluetooth module to go off initially as a reset
@@ -114,14 +105,8 @@ greenOn = False
 redOn = False
 
 def lightSequence(n,a):
-    global runReadSeq
-    global modifyLocations
-    global restartWIFI
-    global allOff
-    global allOn
-    global bleInit
-    global greenOn
-    global redOn
+    global runReadSeq, modifyLocations, restartWIFI, allOff, allOn, bleInit, greenOn, redOn
+
     while True:
         if runReadSeq:
             ble.value = True  #BLE ON/OFF
@@ -193,6 +178,213 @@ _thread.start_new_thread(lightSequence,(1,1)) #start thread for BLE init
 BLEReceived = True
 stopBLEThread = False
 
+
+bleInit = True
+serialPort.write(("AT").encode())
+time.sleep(2)
+serialPort.write(("AT+IMME1").encode())
+time.sleep(2)
+serialPort.write(("AT+NAMELIGHT_IoT").encode())
+time.sleep(2)
+serialPort.write(("AT+IMME0").encode())
+time.sleep(2)
+serialPort.write(("AT+RESET").encode())
+time.sleep(2)
+
+
+print("BLE Initialization Complete")
+bleInit = False
+
+#END BLE Init    
+
+
+
+def runReadSequence():
+    global runReadSeq, modifyLocations, restartWIFI, allOff, allOn, bleInit, greenOn, redOn
+    runReadSeq = True
+    modifyLocations = False
+    restartWIFI = False
+    allOff = False
+    allOn = False
+    bleInit = False
+    greenOn = False
+    redOn = False
+
+    
+    global line
+    global fullString
+    startMsg = "start"
+    endMsg = "stop"
+    ssid = "ssid"
+    ssid_pswd = "password"
+    uid = "uid"
+    dataStart = False
+    dataStop = False
+    line = []
+    while True:
+        for c in serialPort.read().decode():
+            line.append(c)
+            if c == '\n':
+                print("newString")
+                fullString = ''.join(line).replace("\n"," ")
+                print(fullString)
+                line = []
+
+                if startMsg in fullString:
+                    dataStart = True
+                    dataStop = False
+                    print("start")
+                elif endMsg in fullString:
+                    dataStart = False
+                    dataStop = True
+                    print("end")
+                    return
+                elif dataStart == True and dataStop == False:
+                    if ssid in fullString or ssid_pswd in fullString or uid in fullString:
+                        try:
+                            valName = fullString.split(':')[0]
+                            print(valName)
+                            valData = fullString.split(':')[1]
+                            print(valData)
+                            appValues[valName] = valData
+                        except:
+                            print("invalid data")
+                            return
+
+
+
+#restarts the wifi services
+def RestartWifi():
+    global runReadSeq, modifyLocations, restartWIFI, allOff, allOn, bleInit, greenOn, redOn
+    runReadSeq = False
+    modifyLocations = False
+    restartWIFI = True
+    allOff = False
+    allOn = False
+    bleInit = False
+    greenOn = False
+    redOn = False
+
+    os.system('sudo systemctl daemon-reload')
+    time.sleep(5)
+    os.system('sudo systemctl stop dhcpcd.service')
+    time.sleep(5)
+    os.system('sudo systemctl start dhcpcd.service')
+    time.sleep(20)
+
+
+def modifyWPAFile():
+
+    global runReadSeq, modifyLocations, restartWIFI, allOff, allOn, bleInit, greenOn, redOn
+    runReadSeq = False
+    modifyLocations = True
+    restartWIFI = False
+    allOff = False
+    allOn = False
+    bleInit = False
+    greenOn = False
+    redOn = False
+
+    wifiConfig = open("/etc/wpa_supplicant/wpa_supplicant.conf", "r+")
+    fileContents = wifiConfig.readlines()
+    newFileContents = ""
+    currentFileContents = ""
+    for line in fileContents:
+        newLine = line
+        if "ssid" in line:
+            newLine = "        ssid=" + '"'  + appValues.get("ssid").strip() + '"' + "\n"
+            print(newLine)
+        elif "psk" in line:
+            newLine = "        psk=" + '"' + appValues.get("password").strip() + '"' + "\n"
+            print(newLine)
+        newFileContents += newLine
+    wifiConfig.seek(0)
+    wifiConfig.truncate(0)
+    wifiConfig.write(newFileContents)
+    wifiConfig.close()
+    
+    
+    
+def modifyTOKENFile():
+    global runReadSeq, modifyLocations, restartWIFI, allOff, allOn, bleInit, greenOn, redOn, keyFileName
+    runReadSeq = False
+    modifyLocations = True
+    restartWIFI = False
+    allOff = False
+    allOn = False
+    bleInit = False
+    greenOn = False
+    redOn = False
+
+    tokenConfig = open(tokenFileName, "r+")
+    tokenConfig.seek(0)
+    tokenConfig.truncate(0)
+    tokenConfig.write(appValues.get("uid").strip())
+    tokenConfig.close()
+
+    if os.path.exists(keyFileName): #if key exists, remove it
+        os.remove(keyFileName)
+
+
+def bluetoothMAIN():
+    global runReadSeq, modifyLocations, restartWIFI, allOff, allOn, bleInit, greenOn, redOn
+    runReadSeq = False
+    modifyLocations = False
+    restartWIFI = False
+    allOff = False
+    allOn = False
+    bleInit = False
+    greenOn = False
+    redOn = False
+
+
+    print("Bluetooth MAIN")
+    internet = False
+    status = ""
+
+    try:
+        url = "https://www.google.com"
+        #urllib.request.urlopen(url)
+        response = requests.get(url)
+        internet = True
+
+        runReadSeq = False
+        modifyLocations = False
+        restartWIFI = False
+        allOff = False
+        allOn = False
+        bleInit = False
+        greenOn = True
+        redOn = False
+
+        ble.value = False  #BLE ON/OFF
+        status = "Connected"
+    except requests.ConnectionError:
+        status = "Not connected"
+            
+            
+            
+    print(status)
+    if status == "Not connected":
+        
+        runReadSeq = False
+        modifyLocations = False
+        restartWIFI = False
+        allOff = False
+        allOn = False
+        bleInit = False
+        greenOn = False
+        redOn = True
+
+        ble.value = True  #BLE ON/OFF
+        runReadSequence()
+        modifyWPAFile()
+        modifyTOKENFile()
+        RestartWifi()
+        bluetoothMAIN()
+
+
+bluetoothMAIN()
 
 
 #####
@@ -273,311 +465,6 @@ else:
 
 
 
-
-######
-#BLE Init
-
-# BLEReceived = True
-# stopBLEThread = False
-
-
-# def BLEModuleInit(fun,fun1):
-#     line = []
-#     BLEInit = 0
-#     global BLEReceived
-#     while stopBLEThread == False:
-#         for c in serialPort.read().decode():
-#             line.append(c)
-#             linev2 = ''.join(line).replace("\n", " ").strip()
-#             if linev2 == "OK+Set:1":
-#                 print(linev2)
-#                 line = []
-#                 BLEReceived = True
-#             elif linev2 == "OK+RESET":
-#                 print(linev2)
-#                 line = []
-#                 BLEReceived = True
-#             elif linev2 == "OK+Set:0":
-#                 print(linev2)
-#                 line = []
-#                 BLEReceived = True
-#             elif linev2 == "OK+RESET":
-#                 print(linev2)
-#                 line = []
-#                 BLEReceived = True
-#             elif linev2 == "OK+Set:SERVER_IoT":
-#                 print(linev2)
-#                 line = []
-#                 BLEReceived = True
-#             elif linev2 == "OK" and BLEInit == 0:
-#                 BLEInit = 1
-#                 print(linev2)
-#                 line = []
-#                 BLEReceived = True
-
-# _thread.start_new_thread(BLEModuleInit,(1,1)) #start thread for BLE init
-
-
-bleInit = True
-# BLEReceived = False
-serialPort.write(("AT").encode())
-time.sleep(2)
-# while BLEReceived == False: continue
-# BLEReceived = False
-serialPort.write(("AT+IMME1").encode())
-time.sleep(2)
-# while BLEReceived == False: continue
-# BLEReceived = False
-serialPort.write(("AT+NAMELIGHT_IoT").encode())
-time.sleep(2)
-# while BLEReceived == False: continue
-# BLEReceived = False
-serialPort.write(("AT+IMME0").encode())
-time.sleep(2)
-# while BLEReceived == False: continue
-# BLEReceived = False
-serialPort.write(("AT+RESET").encode())
-time.sleep(2)
-# while BLEReceived == False: continue
-
-
-# stopBLEThread = True
-print("BLE Initialization Complete")
-bleInit = False
-
-#END BLE Init    
-
-
-
-def runReadSequence():
-    global runReadSeq
-    global modifyLocations
-    global restartWIFI
-    global allOff
-    global allOn
-    global bleInit
-    global greenOn
-    global redOn
-    runReadSeq = True
-    modifyLocations = False
-    restartWIFI = False
-    allOff = False
-    allOn = False
-    bleInit = False
-    greenOn = False
-    redOn = False
-
-    
-    global line
-    global fullString
-    startMsg = "start"
-    endMsg = "stop"
-    ssid = "ssid"
-    ssid_pswd = "password"
-    uid = "uid"
-    dataStart = False
-    dataStop = False
-    line = []
-    while True:
-        for c in serialPort.read().decode():
-            line.append(c)
-            if c == '\n':
-                print("newString")
-                fullString = ''.join(line).replace("\n"," ")
-                print(fullString)
-                line = []
-
-                if startMsg in fullString:
-                    dataStart = True
-                    dataStop = False
-                    print("start")
-                elif endMsg in fullString:
-                    dataStart = False
-                    dataStop = True
-                    print("end")
-                    return
-                elif dataStart == True and dataStop == False:
-                    if ssid in fullString or ssid_pswd in fullString or uid in fullString:
-                        try:
-                            valName = fullString.split(':')[0]
-                            print(valName)
-                            valData = fullString.split(':')[1]
-                            print(valData)
-                            appValues[valName] = valData
-                        except:
-                            print("invalid data")
-                            return
-
-
-
-#restarts the wifi services
-def RestartWifi():
-    global runReadSeq
-    global modifyLocations
-    global restartWIFI
-    global allOff
-    global allOn
-    global bleInit
-    global greenOn
-    global redOn
-    runReadSeq = False
-    modifyLocations = False
-    restartWIFI = True
-    allOff = False
-    allOn = False
-    bleInit = False
-    greenOn = False
-    redOn = False
-
-    os.system('sudo systemctl daemon-reload')
-    time.sleep(5)
-    os.system('sudo systemctl stop dhcpcd.service')
-    time.sleep(5)
-    os.system('sudo systemctl start dhcpcd.service')
-    time.sleep(20)
-
-
-def modifyWPAFile():
-    global runReadSeq
-    global modifyLocations
-    global restartWIFI
-    global allOff
-    global allOn
-    global bleInit
-    global greenOn
-    global redOn
-    runReadSeq = False
-    modifyLocations = True
-    restartWIFI = False
-    allOff = False
-    allOn = False
-    bleInit = False
-    greenOn = False
-    redOn = False
-
-    wifiConfig = open("/etc/wpa_supplicant/wpa_supplicant.conf", "r+")
-    fileContents = wifiConfig.readlines()
-    newFileContents = ""
-    currentFileContents = ""
-    for line in fileContents:
-        newLine = line
-        if "ssid" in line:
-            newLine = "        ssid=" + '"'  + appValues.get("ssid").strip() + '"' + "\n"
-            print(newLine)
-        elif "psk" in line:
-            newLine = "        psk=" + '"' + appValues.get("password").strip() + '"' + "\n"
-            print(newLine)
-        newFileContents += newLine
-    wifiConfig.seek(0)
-    wifiConfig.truncate(0)
-    wifiConfig.write(newFileContents)
-    wifiConfig.close()
-    
-    
-    
-def modifyTOKENFile():
-    global runReadSeq
-    global modifyLocations
-    global restartWIFI
-    global allOff
-    global allOn
-    global bleInit
-    global greenOn
-    global redOn
-    runReadSeq = False
-    modifyLocations = True
-    restartWIFI = False
-    allOff = False
-    allOn = False
-    bleInit = False
-    greenOn = False
-    redOn = False
-
-    tokenConfig = open(tokenFileName, "r+")
-    tokenConfig.seek(0)
-    tokenConfig.truncate(0)
-    tokenConfig.write(appValues.get("uid").strip())
-    tokenConfig.close()
-
-######
-
-
-
-
-def bluetoothMAIN():
-    global runReadSeq
-    global modifyLocations
-    global restartWIFI
-    global allOff
-    global allOn
-    global bleInit
-    global greenOn
-    global redOn
-    runReadSeq = False
-    modifyLocations = False
-    restartWIFI = False
-    allOff = False
-    allOn = False
-    bleInit = False
-    greenOn = False
-    redOn = False
-
-
-    print("Bluetooth MAIN")
-    internet = False
-    status = ""
-    #check if the pi is connected to the internet'
-    #while internet == False:
-    try:
-        url = "https://www.google.com"
-        #urllib.request.urlopen(url)
-        response = requests.get(url)
-        internet = True
-        runReadSeq = False
-        modifyLocations = False
-        restartWIFI = False
-        allOff = False
-        allOn = False
-        bleInit = False
-        greenOn = True
-        redOn = False
-        ble.value = False  #BLE ON/OFF
-        status = "Connected"
-    except requests.ConnectionError:
-        #print(response.status_code)
-        status = "Not connected"
-            
-            
-            
-    print(status)
-    if status == "Not connected":
-        runReadSeq = False
-        modifyLocations = False
-        restartWIFI = False
-        allOff = False
-        allOn = False
-        bleInit = False
-        greenOn = False
-        redOn = True
-        ble.value = True  #BLE ON/OFF
-        runReadSequence()
-        modifyWPAFile()
-        modifyTOKENFile()
-        RestartWifi()
-        bluetoothMAIN()
-
-
-bluetoothMAIN()
-
-# Used to decrypt the file, we only want to decrypt the contents
-
-# Opening the file in write mode and
-# Writing the decrypted data
-# with open('token.txt', 'wb') as dec_file:
-# 	dec_file.write(decrypted)
-
-
-
 ##DATABASE INIT##
 #firebase database config
 config = {
@@ -622,8 +509,7 @@ def sendingToDatabase(data):
     #send the data to the database
     database.child((decryptFileContents(tokenFileName, key)).decode("utf-8") + "/dataFromChild").update({str(deviceName) : str(data)})
 
-    
-        
+
 def sendSampleThread(sendSocket,receive):
     global interval
     global average_lux
@@ -631,22 +517,6 @@ def sendSampleThread(sendSocket,receive):
         time.sleep(interval)
         print('average_lux: ' + str(average_lux))
         sendingToDatabase(average_lux)
-
-
-
-#hardware SPI configuration
-#SPI_PORT   = 0
-#SPI_DEVICE = 0
-#connects the SPI port and device to the variable
-#mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-#set the GPIO to the board layout (used for pin numbers)
-# GPIO.setmode(GPIO.BOARD)
-#set the GPIO pin 18 to output
-# GPIO.setup(18, GPIO.OUT)
-
-
-
 
 
 # Find the firebase token, and verify it is correct
