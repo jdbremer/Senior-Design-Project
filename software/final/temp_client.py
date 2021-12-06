39bae7c8b9668ff1b04790a68a07859aa9a915d6
Jordan <jordan.bremer@yahoo.com>
2021-12-05 22:17:53 -0600

    temp client rev 1

--- software/final/temp_client.py
+++ software/final/temp_client.py
@@ -23,22 +23,22 @@ import pyrebase
 from cryptography.fernet import Fernet
 
 # import busio
-# import digitalio
+import digitalio
 # import board
 # import adafruit_mcp3xxx.mcp3008 as MCP
 # from adafruit_mcp3xxx.analog_in import AnalogIn
 
 # create the spi bus
-# spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
 # create the cs (chip select)
-# cs = digitalio.DigitalInOut(board.D5)
+cs = digitalio.DigitalInOut(board.D5)
 
 # create the mcp object
-# mcp = MCP.MCP3008(spi, cs)
+mcp = MCP.MCP3008(spi, cs)
 
 # create an analog input channel on pin 0
-# chan = AnalogIn(mcp, MCP.P0)
+chan = AnalogIn(mcp, MCP.P0)
 
 
 mac = get_mac()
